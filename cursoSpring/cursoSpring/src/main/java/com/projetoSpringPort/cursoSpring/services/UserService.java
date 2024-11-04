98bed522ac507188a24adb2ff9968c48ad363f6c
Bianco <arthnilo@gmail.com>
2024-11-04 09:57:29 -0300

    Service Layer, component registration

---
+++ cursoSpring/cursoSpring/src/main/java/com/projetoSpringPort/cursoSpring/services/UserService.java
@@ -0,0 +1,24 @@
+package com.projetoSpringPort.cursoSpring.services;
+
+import com.projetoSpringPort.cursoSpring.entities.User;
+import com.projetoSpringPort.cursoSpring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() { return repository.findAll(); }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
